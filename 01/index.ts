09253f021b6bd6c894538120d69ec6d898a105a6
Andrew Davis <dynamicdebug@gmail.com>
2024-12-10 15:20:04 -0500

    part two day one in the books

--- 01/index.ts
+++ 01/index.ts
@@ -18,11 +18,34 @@ async function partOne() {
     .reduce((a, b) => {
       return a + b;
     }, 0);
-  
 }
-const a = await partOne();
-console.log(a)
-
+// const a = await partOne();
+// console.log(a);
 
 /* part two */
 
+async function partTwo() {
+  const data = await Deno.readTextFile('./part_one.txt');
+  const lines = data.split('\n');
+  let col1: number[] = [];
+  let col2: number[] = [];
+  lines.forEach((line) => {
+    const nums = line.trim().split(/\s+/).map(Number);
+    col1.push(nums[0]);
+    col2.push(nums[1]);
+  });
+  let sortedColumnOnePartTwo = [...col1];
+
+  let tot = 0;
+  for (const [, value] of sortedColumnOnePartTwo.entries()) {
+    // look for count of value in column two
+    let targetCount = findNumCount(col2, value);
+    let total = targetCount.length * value;
+    tot += total;
+  }
+
+}await partTwo();
+
+function findNumCount(array: number[], target: number) {
+  return array.filter((x) => x === target);
+}
