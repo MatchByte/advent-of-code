e8928849ee0d2aa7f1169800f4548ac483ef1df5
Andrew Davis <adavis@dmwdirect.com>
2024-12-10 14:23:54 -0500

    part one day one better late than never

---
+++ 01/index.ts
@@ -0,0 +1,28 @@
+async function partOne() {
+  const data = await Deno.readTextFile('./part_one.txt');
+  const lines = data.split('\n');
+  let col1: number[] = [];
+  let col2: number[] = [];
+  lines.forEach((line, i) => {
+    const nums = line.trim().split(/\s+/).map(Number);
+    col1.push(nums[0]);
+    col2.push(nums[1]);
+  });
+
+  const sortedColumnOne = [...col1].sort((a, b) => a - b);
+  const sortedColumnTwo = [...col2].sort((a, b) => a - b);
+  return sortedColumnTwo
+    .map((element, index) => {
+      return Math.abs(sortedColumnOne[index] - element);
+    })
+    .reduce((a, b) => {
+      return a + b;
+    }, 0);
+  
+}
+const a = await partOne();
+console.log(a)
+
+
+/* part two */
+
