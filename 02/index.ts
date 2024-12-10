b84821f31a7a37616bb91a3f50a259fd857500b0
Andrew Davis <dynamicdebug@gmail.com>
2024-12-10 17:18:39 -0500

    day two part one work

--- 02/index.ts
+++ 02/index.ts
@@ -1,7 +1,13 @@
 async function _partOne() {
   const input = await Deno.readTextFile('./input.txt');
-  const _lines = input.trim().split('\n');
-  console.log(_lines);
-  
+  const _lines = input
+    .trim()
+    .split('\n')
+    .map((line) => [...line.split(/\s+/).map(Number)]);
+
+  for (const [indx,val] of _lines.entries()) {
+    const chunks = val.slice(indx, indx + 2);
+    console.log(chunks);
+  }
 }
-await _partOne()
+await _partOne();
