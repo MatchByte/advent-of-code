83a081a5b54b07a5463036b1877e544b59299864
Andrew Davis <dynamicdebug@gmail.com>
2024-12-11 22:00:24 -0500

    2

--- 02/index.ts
+++ 02/index.ts
@@ -5,9 +5,9 @@ async function _partOne() {
     .split('\n')
     .map((line) => [...line.split(/\s+/).map(Number)]);
 
-  for (const [indx,val] of _lines.entries()) {
+  for (const [indx, val] of _lines.entries()) {
     const chunks = val.slice(indx, indx + 2);
     console.log(chunks);
   }
 }
-await _partOne();
+
