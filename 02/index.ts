eeda6ea6371c707aa8aad6d98f38fc7389cfc863
Andrew Davis <dynamicdebug@gmail.com>
2024-12-13 17:18:45 -0500

    helper function for day 2

--- 02/index.ts
+++ 02/index.ts
@@ -1,7 +1,20 @@
-async function _partOne() {
+// deno-lint-ignore-file
+async function partOne() {
   const input = await Deno.readTextFile('./input.txt');
-  const _lines = input
-    .trim()
-    .split('\n')
-    .map((line) => [...line.split(/\s+/).map(Number)]);
+  const lines = input.trim().split('\n').map((line => line.trim().split(/\s+/)));
+  console.log({lines});
+  let testArray = [7, 6, 4, 2, 1];
+  let pairs = getArrayPairs(testArray);
+
+}
+
+await partOne();
+
+function getArrayPairs(array) {
+  return array.reduce((pairs, currentValue, i) => {
+    if (i < array.length - 1) {
+      pairs.push([currentValue, array[i + 1]]);
+    }
+    return pairs;
+  }, []);
 }
